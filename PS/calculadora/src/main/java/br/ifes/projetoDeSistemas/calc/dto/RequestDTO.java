e6be88c14c142cfac994e4c7965d86c8b0601829
jramso <josue.rsou2@gmail.com>
2024-09-30 21:34:49 -0300

    feat: DTO getter ans setter

--- PS/calculadora/src/main/java/br/ifes/projetoDeSistemas/calc/dto/RequestDTO.java
+++ PS/calculadora/src/main/java/br/ifes/projetoDeSistemas/calc/dto/RequestDTO.java
@@ -1,5 +1,35 @@
 package br.ifes.projetoDeSistemas.calc.dto;
 
 public class RequestDTO {
+    private int opcao;
+    private int valor1;
+    private int valor2;
+
+    public RequestDTO(int opcao, int valor1, int valor2) {
+        this.opcao = opcao;
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    public int getOpcao() {
+        return opcao;
+    }
+    public void setOpcao(int opcao) {
+        this.opcao = opcao;
+    }
+
+    public int getValor1() {
+        return valor1;
+    }
+    public void setValor1(int valor1) {
+        this.valor1 = valor1;
+    }
+    public int getValor2() {
+        return valor2;
+    }
+    public void setValor2(int valor2) {
+        this.valor2 = valor2;
+    }
+
 
 }
--- PS/calculadora/src/main/java/br/ifes/projetoDeSistemas/calc/dto/ResponseDTO.java
+++ PS/calculadora/src/main/java/br/ifes/projetoDeSistemas/calc/dto/ResponseDTO.java
@@ -2,4 +2,17 @@ package br.ifes.projetoDeSistemas.calc.dto;
 
 public class ResponseDTO {
 
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    private int result = 0;
+
+    public ResponseDTO(int result) {
+        this.result = result;
+    }
 }
